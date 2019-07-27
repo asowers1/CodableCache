6f80c449d9b697daf6358595bd2bcb060d35a363
Joe Fabisevich <mergesort@users.noreply.github.com>
2019-07-27 18:46:03 -0400

    Updating Package.swift to use the modern format

--- Package.swift
+++ Package.swift
@@ -1,5 +1,22 @@
 import PackageDescription
 
 let package = Package(
-    name: "CodableCache"
+    name: "CodableCache",
+    platforms: [
+        .iOS(.v9),
+        .macOS(.v10_10),
+        .tvOS(.v9),
+        .watchOS(.v3)
+    ],
+    products: [
+        .library(
+            name: "CodableCache",
+            targets: ["CodableCache"]),
+    ],
+    targets: [
+        .target(
+            name: "CodableCache",
+            dependencies: []
+        ),
+    ]
 )
